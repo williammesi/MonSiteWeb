e34b08d9e9cb885984dfb661b9ac7958293209ff
William <2246747@cstj.qc.ca>
2025-10-10 15:35:56 -0400

    test 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,14 +4,14 @@ defineProps({
     type: String,
     required: true,
   },
-})
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
+      You’ve successfully mouhaha created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
